769291e7a9faae037c7d63337f0d902049a669a4
LDPCICNM <78750096+LDPCICNM@users.noreply.github.com>
2021-03-23 21:07:22 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,12 @@
 input.onButtonPressed(Button.A, function () {
-    let sprite: game.LedSprite = null
-    if (sprite.get(LedSpriteProperty.X) == 2) {
+    if (scrip.get(LedSpriteProperty.X) == 2) {
         game.addScore(1)
     } else {
         game.gameOver()
     }
 })
-let scrip = game.createSprite(2, 2)
+let scrip: game.LedSprite = null
+scrip = game.createSprite(2, 2)
 basic.forever(function () {
     scrip.move(1)
     scrip.ifOnEdgeBounce()
